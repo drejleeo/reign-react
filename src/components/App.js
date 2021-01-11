f59c5b1dfde73b505e884cfdc05fd96443a12344
aleonte <andrei.leonte.p@thalesdigital.io>
2021-01-11 14:09:28 +0200

    Finally gh pages are working

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/reign-react">
       <Nav />
       <div className="main-panel">
         <Switch>

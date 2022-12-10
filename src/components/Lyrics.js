51d4e1eae4d9a8e60b77ef32952c6b217ee938aa
drejleeo <drejleeo@gmail.com>
2022-12-10 14:26:23 +0200

    Add forgotten edits. Must be revised

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -23,6 +23,7 @@ class LyricsCreate extends Component {
     constructor(props) {
         super(props);
         this.state = { };
+        this.media = '';
 
         this.videoProps = {
             ratio: '16/9',
@@ -32,27 +33,18 @@ class LyricsCreate extends Component {
         document.documentElement.style.setProperty('--size-nb-side-width', this.videoProps.width);
     }
 
-    computeVideoHeight() {
-        const ratio = this.videoProps.ratio.split('/');
-        return this.videoProps.width * ratio[1] / ratio[0];
-    }
-
-    getVideoWidth() {
-        console.log($('nb-side-panel').clientWidth);
-        return $('nb-side-panel').clientWidth;
-    }
-
-    getVideoHeight() {
-        console.log($('nb-side-panel').clientHeight);
-        return $('nb-side-panel').clientHeight;
+    renderYouTubeMedia = event => {
+        $('#video-frame').attr("src", event.target.value);
+        this.media = event.target.value;
+        console.log(this.media);
     }
 
     render() {
         return (
             <div className="notebook row">
                 <div className="nb-page col-8 xy-center full-min-height">
-                    <div className="nb-format-lines">
-                        <input className="title-input no-focus no-border abril-fatface" placeholder="Add title"></input>
+                    <div className="nb-article">
+                        <input className="nb-title no-focus no-border abril-fatface" placeholder="Add title"></input>
                         <Editor
                             className="no-focus no-border"
                             apiKey={tinyApiKey}
@@ -76,15 +68,16 @@ class LyricsCreate extends Component {
                         />
                     </div>
                 </div>
+                {/* Try this: https://www.youtube-nocookie.com/embed/videoseries?list=PLAkpLQE6lk-hLnBjXLa3arXiM7ebcAOTe */}
                 <div className="nb-side-panel col-4">
                     <div className="video-container">
-                        <iframe title="soundtrack" className="video-beat-frame" src="https://www.youtube-nocookie.com/embed/videoseries?list=PLAkpLQE6lk-hLnBjXLa3arXiM7ebcAOTe"
+                        <iframe title="soundtrack" id="video-frame" src={this.media}
                             frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen>
                         </iframe>
                     </div>
                     <div className="nb-side-meta xy-center">
-                        <input className="no-focus no-border" placeholder="Add sounds here"></input>
+                        <input id="add-media" className="no-focus no-border" placeholder="Add sounds here" onChange={this.renderYouTubeMedia}></input>
                     </div>
                 </div>
             </div>
